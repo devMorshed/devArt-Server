b3bfadc777794b796190d1623452053c164c5ef5
devMorshed <ac3m0r@gmail.com>
2023-06-09 16:23:54 +0600

    cart delete api

--- index.js
+++ index.js
@@ -79,6 +79,20 @@ async function run() {
 		res.send(result);
 	});
 
+	app.get("/cart", async (req, res) => {
+		const email = req.query.email;
+		const query = { email: email };
+		const result = await cartCollection.find(query).toArray();
+		res.send(result);
+	});
+
+	app.delete("/cart/:id", async (req, res) => {
+		const id = req.params.id;
+		const query = { _id: new ObjectId(id) };
+		const result = await cartCollection.deleteOne(query);
+		res.send(result);
+	});
+
 	// Send a ping to confirm a successful connection
 	await client.db("admin").command({ ping: 1 });
 	console.log("Connected to MongoDB!");
