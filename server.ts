e98676a14b07ec7165127a3fadcacd1d492d3394
devMorshed <ac3m0r@gmail.com>
2024-02-03 23:34:54 +0600

    test, catch all route enabled

--- server.ts
+++ server.ts
@@ -1,10 +1,7 @@
 import { app } from "./app";
 require("dotenv").config();
 
-// testing route
-app.get("/test", (req, res, next) => {
-  return res.json({ message: "Test route working" });
-});
+
 
 // Server creation
 app.listen(process.env.PORT, () => {
